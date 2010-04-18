1dd5c0825661f2ce1ac7230a9f7d1eec5b615978
Andrea Censi <andrea@cds.caltech.edu>
2010-04-18 22:14:44 +0000

    New tests for pickling and json encoding

--- covariance/go.py
+++ covariance/go.py
@@ -1,9 +1,11 @@
 from pybv.worlds import create_random_world, get_safe_pose
-from pybv.sensors import ImageRangeSensor, TexturedRaytracer, OlfactionSensor
+from pybv.sensors import ImageRangeSensor, \
+  TexturedRaytracer, OlfactionSensor
 from pybv.utils import RigidBodyState
 from pybv.vehicle import Vehicle
 from pybv.simulation import random_pose_simulation
-from pybv_experiments import create_uniform_sensor, create_example_nonuniform
+from pybv_experiments import create_uniform_sensor, \
+  create_example_nonuniform, create_ring_olfaction_sensor
 from numpy import deg2rad, pi, random
 from numpy.linalg import norm
 import sys
@@ -11,7 +13,8 @@ import sys
 world_radius = 10
 world = create_random_world(radius=world_radius)
 
-sensor_uniform = create_uniform_sensor(fov_deg=360, num_rays=180, spatial_sigma_deg=0.5, sigma=0.01)
+sensor_uniform = create_uniform_sensor(fov_deg=360, num_rays=180,
+                               spatial_sigma_deg=0.5, sigma=0.01)
 
 sensor_uniform.set_map(world)
 
@@ -30,7 +33,8 @@ num_iterations = 100
 if 'many' in sys.argv:
     num_iterations = 10000
 
-from pybv_experiments.covariance import LuminanceCovariance, ReadingsCovariance,SenselCovariance
+from pybv_experiments.covariance \
+ import LuminanceCovariance, ReadingsCovariance,SenselCovariance
 
 all_jobs = {}
 sensors = {'uniform': sensor_uniform, 'nonuniform': sensor_nonuniform } 
@@ -42,13 +46,17 @@ for sensor_name, sensor in sensors.items():
     vehicle.add_optic_sensor(sensor)
     
     job_id = 'covariance_luminance_%s' % sensor_name
-    all_jobs[job_id] = random_pose_simulation(job_id=job_id, world=world, vehicle=vehicle, random_pose_gen=random_pose_gen, num_iterations=num_iterations, processing_class=LuminanceCovariance) 
+    all_jobs[job_id] = random_pose_simulation(job_id=job_id, 
+     world=world, vehicle=vehicle, random_pose_gen=random_pose_gen, 
+     num_iterations=num_iterations, processing_class=LuminanceCovariance) 
 
     # second try: only ranges
     vehicle2 = Vehicle()
     vehicle2.add_rangefinder(sensor)
     job_id = 'covariance_distance_%s' % sensor_name
-    all_jobs[job_id] = random_pose_simulation(job_id=job_id, world=world, vehicle=vehicle2, random_pose_gen=random_pose_gen, num_iterations=num_iterations, processing_class=ReadingsCovariance)
+    all_jobs[job_id] = random_pose_simulation(job_id=job_id, world=world, 
+        vehicle=vehicle2, random_pose_gen=random_pose_gen, 
+        num_iterations=num_iterations, processing_class=ReadingsCovariance)
 
     # third try: both ranges and luminance
     vehicle3 = Vehicle()
@@ -56,5 +64,18 @@ for sensor_name, sensor in sensors.items():
     vehicle3.add_rangefinder(sensor)
 
     job_id = 'covariance_sensels_%s' % sensor_name
-    all_jobs[job_id] = random_pose_simulation(job_id=job_id, world=world, vehicle=vehicle3,  random_pose_gen=random_pose_gen, num_iterations=num_iterations, processing_class=SenselCovariance)
+    all_jobs[job_id] = random_pose_simulation(job_id=job_id, world=world, 
+        vehicle=vehicle3,  random_pose_gen=random_pose_gen, 
+        num_iterations=num_iterations, processing_class=SenselCovariance)    
+
+# olfaction sensor
+
+vehicle = Vehicle()
+of = create_ring_olfaction_sensor(fov_deg=180, num_sensors=40, radius=0.3)
+of.set_map(world)
+vehicle.add_olfaction_sensor(of)
+job_id = 'covariance_olfaction' 
+all_jobs[job_id] = random_pose_simulation(job_id=job_id, world=world, 
+        vehicle=vehicle,  random_pose_gen=random_pose_gen, 
+        num_iterations=num_iterations, processing_class=SenselCovariance)
 
--- pieces.py
+++ pieces.py
@@ -1,13 +1,15 @@
-""" Standard pieces """
+""" Standard pieces used in the experiments """
+from numpy import deg2rad, pi, linspace, array, cos, sin
 
-from pybv.sensors import ImageRangeSensor
-from numpy import deg2rad, pi, linspace
+from pybv.sensors import ImageRangeSensor, OlfactionSensor
+from pybv.utils import RigidBodyState
 
 def create_uniform_sensor(fov_deg, num_rays, spatial_sigma_deg=0.5, sigma=0.01):
+    """ Creates a uniform image/range sensor whose rays are uniformly distributed """
     sensor = ImageRangeSensor()
-    sensor.add_photoreceptors(deg2rad(linspace(-fov_deg/2,fov_deg/2, num_rays)), spatial_sigma=deg2rad(spatial_sigma_deg), sigma=sigma)
+    directions = deg2rad(linspace(-fov_deg/2,fov_deg/2, num_rays))
+    sensor.add_photoreceptors(directions, spatial_sigma=deg2rad(spatial_sigma_deg), sigma=sigma)
     return sensor
-    
 
 def create_example_nonuniform():
     """ Design the following sensor:
@@ -21,4 +23,14 @@ def create_example_nonuniform():
     sensor.add_photoreceptors(deg2rad(range(+30,+90,3)), spatial_sigma=deg2rad(2), sigma=0.01)
     sensor.add_photoreceptors(deg2rad(range(-90,-270,3)), spatial_sigma=deg2rad(5), sigma=0.01)
     return sensor
+    
+def create_ring_olfaction_sensor(fov_deg, num_sensors, radius):
+    """ Creates an olfaction sensor whose receptors are set in a ring"""
+    of = OlfactionSensor()
+    directions = deg2rad(linspace(-fov_deg/2,fov_deg/2, num_sensors))
+    for theta in directions:
+        position = array([cos(theta), sin(theta)]) * radius
+        sensitivity = {'food': 1}
+        of.add_receptor( pose=RigidBodyState(position=position), sensitivity=sensitivity)
+    return of
     
