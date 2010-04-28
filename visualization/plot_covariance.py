e7f141ef95db0fafd2a3b426ae74dbebf85d3e83
Andrea Censi <andrea@cds.caltech.edu>
2010-04-28 19:18:23 +0000

    removed old files

--- visualization/plot_covariance.py
+++ visualization/plot_covariance.py
@@ -1,22 +1,22 @@
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-import matplotlib.pyplot as plt
-import numpy as np
-
-
-def plot_covariance(covariance, directions=None):
+if 0:
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib import cm
+    import matplotlib.pyplot as plt
+    import numpy as np
     
-    fig = plt.figure()
-    ax = Axes3D(fig)
-
-    n = covariance.shape[0]
-    if directions is None:
-        directions = range(1, n)
+    def plot_covariance(covariance, directions=None):
         
-    X = directions
-    Y = directions
-    X, Y = np.meshgrid(X, Y)
-    Z = covariance
-    ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet)
-
-    plt.show()
+        fig = plt.figure()
+        ax = Axes3D(fig)
+    
+        n = covariance.shape[0]
+        if directions is None:
+            directions = range(1, n)
+            
+        X = directions
+        Y = directions
+        X, Y = np.meshgrid(X, Y)
+        Z = covariance
+        ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet)
+    
+        plt.show()
