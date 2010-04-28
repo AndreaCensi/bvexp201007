41c5bc4bd5360fb5ce04a7755c66b960c8eee986
Andrea Censi <andrea@cds.caltech.edu>
2010-04-28 19:16:26 +0000

    removed old files

--- parsim_all.py
+++
@@ -1,7 +0,0 @@
-
-# Load all the computations
-import pybv_experiments.first_order.go_parallel
-
-from compmake import interpret_commands
-
-interpret_commands()
--- visualization/plot_covariance.py
+++ visualization/plot_covariance.py
@@ -1,9 +1,7 @@
-if 0:
-
-	from mpl_toolkits.mplot3d import Axes3D
-	from matplotlib import cm
-	import matplotlib.pyplot as plt
-	import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_covariance(covariance, directions=None):
