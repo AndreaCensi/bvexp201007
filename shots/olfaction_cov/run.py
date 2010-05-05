3cbe835af7c06b128446f31b703ae8482e02aca8
Andrea Censi <andrea@cds.caltech.edu>
2010-05-05 16:07:36 +0000

    added mean normalization

--- shots/olfaction_cov/run.py
+++ shots/olfaction_cov/run.py
@@ -5,8 +5,10 @@ from pybv_experiments.analysis.olfaction_tensors import \
 
 if __name__ == '__main__':
     files = [
-        ('v_olfaction-random_pose_simulation.pickle', 'v_olfaction_'),
-        ('v_olfaction_norm-random_pose_simulation.pickle', 'v_olfaction_norm_')
+        ('v_olfaction-random_pose_simulation.pickle', 'v_olfaction180o_'),
+        ('v_olfaction_norm-random_pose_simulation.pickle', 'v_olfaction180n_'),
+        ('v_olfaction360-random_pose_simulation.pickle', 'v_olfaction360o_'),
+        ('v_olfaction360_norm-random_pose_simulation.pickle', 'v_olfaction360n_')
              ]
 
     path = ['olfaction_cov']
