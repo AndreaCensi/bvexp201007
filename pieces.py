236817115ddd15f858d574544a4f1e2d0eadf27a
Andrea Censi <andrea@cds.caltech.edu>
2010-05-05 16:10:43 +0000

    new sensor names and whitespace

--- pieces.py
+++ pieces.py
@@ -4,10 +4,14 @@ from numpy import deg2rad
 from pybv.sensors import *
 from pybv.vehicle import Vehicle, OmnidirectionalKinematics
 
-def create_uniform_sensor(sensor, fov_deg, num_rays, spatial_sigma_deg=0.5, sigma=0.01):
-    """ Creates a uniform image/range sensor whose rays are uniformly distributed """
+def create_uniform_sensor(sensor, fov_deg, num_rays, spatial_sigma_deg=0.5,
+                          sigma=0.01):
+    """ Creates a uniform image/range sensor whose rays are 
+        uniformly distributed """
     directions = deg2rad(linspace(-fov_deg / 2, fov_deg / 2, num_rays))
-    sensor.add_photoreceptors(directions, spatial_sigma=deg2rad(spatial_sigma_deg), sigma=sigma)
+    sensor.add_photoreceptors(directions,
+                              spatial_sigma=deg2rad(spatial_sigma_deg),
+                              sigma=sigma)
     return sensor
 
 def create_example_nonuniform(sensor):
@@ -16,10 +20,14 @@ def create_example_nonuniform(sensor):
         * sparse side view   [-90 : 3 : -30]  U [30 : 3 : 90]  spatial_sigma=1
         * sparse & ??? backward  view   [-90 : 3 : -270]  spatial_sigma=3
     """
-    sensor.add_photoreceptors(deg2rad(range(-30, 30, 1)), spatial_sigma=deg2rad(2), sigma=0.01)
-    sensor.add_photoreceptors(deg2rad(range(-90, -30, 3)), spatial_sigma=deg2rad(2), sigma=0.01)
-    sensor.add_photoreceptors(deg2rad(range(+30, +90, 3)), spatial_sigma=deg2rad(2), sigma=0.01)
-    sensor.add_photoreceptors(deg2rad(range(-90, -270, 3)), spatial_sigma=deg2rad(5), sigma=0.01)
+    sensor.add_photoreceptors(deg2rad(range(-30, 30, 1)),
+                              spatial_sigma=deg2rad(2), sigma=0.01)
+    sensor.add_photoreceptors(deg2rad(range(-90, -30, 3)),
+                              spatial_sigma=deg2rad(2), sigma=0.01)
+    sensor.add_photoreceptors(deg2rad(range(+30, +90, 3)),
+                              spatial_sigma=deg2rad(2), sigma=0.01)
+    sensor.add_photoreceptors(deg2rad(range(-90, -270, 3)),
+                              spatial_sigma=deg2rad(5), sigma=0.01)
     return sensor
     
 def create_ring_olfaction_sensor(fov_deg, num_sensors, radius):
@@ -29,7 +37,8 @@ def create_ring_olfaction_sensor(fov_deg, num_sensors, radius):
     for theta in directions:
         position = array([cos(theta), sin(theta)]) * radius
         sensitivity = {'food': 1}
-        of.add_receptor(pose=RigidBodyState(position=position), sensitivity=sensitivity)
+        of.add_receptor(pose=RigidBodyState(position=position),
+                        sensitivity=sensitivity)
     return of
     
     
@@ -83,29 +92,33 @@ def vehicles_list_A():
 
     vehicle = Vehicle()
     vehicle.set_dynamics(kin)
-    sensor = create_ring_olfaction_sensor(fov_deg=180, num_sensors=40, radius=0.3)
+    sensor = create_ring_olfaction_sensor(fov_deg=180, num_sensors=40,
+                                          radius=0.3)
     vehicle.add_sensor(sensor)
-    vlist.append(('v_olfaction', vehicle))
+    vlist.append(('v_olfaction180o', vehicle))
 
     vehicle = Vehicle()
     vehicle.set_dynamics(kin)
-    sensor = create_ring_olfaction_sensor(fov_deg=180, num_sensors=40, radius=0.3)
-    sensor.normalize = True
+    sensor = create_ring_olfaction_sensor(fov_deg=180, num_sensors=40,
+                                          radius=0.3)
+    sensor.normalize_mean = True
     vehicle.add_sensor(sensor)
-    vlist.append(('v_olfaction_norm', vehicle))
+    vlist.append(('v_olfaction180n', vehicle))
 
     vehicle = Vehicle()
     vehicle.set_dynamics(kin)
-    sensor = create_ring_olfaction_sensor(fov_deg=360, num_sensors=40, radius=0.3)
+    sensor = create_ring_olfaction_sensor(fov_deg=360, num_sensors=40,
+                                          radius=0.3)
     vehicle.add_sensor(sensor)
-    vlist.append(('v_olfaction360', vehicle))
+    vlist.append(('v_olfaction360o', vehicle))
 
     vehicle = Vehicle()
     vehicle.set_dynamics(kin)
-    sensor = create_ring_olfaction_sensor(fov_deg=360, num_sensors=40, radius=0.3)
-    sensor.normalize = True
+    sensor = create_ring_olfaction_sensor(fov_deg=360, num_sensors=40,
+                                          radius=0.3)
+    sensor.normalize_mean = True
     vehicle.add_sensor(sensor)
-    vlist.append(('v_olfaction360_norm', vehicle))
+    vlist.append(('v_olfaction360n', vehicle))
 
     
     vehicle = Vehicle()
