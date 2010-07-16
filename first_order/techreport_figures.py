e658273de509c44a8c2c9f0386fae69455fad604
Andrea Censi <andrea@cds.caltech.edu>
2010-07-16 20:11:49 +0000

    Report

--- first_order/techreport_figures.py
+++ first_order/techreport_figures.py
@@ -1,40 +1,59 @@
-from reprep.out.platex import Latex, latexify
+from reprep.out.platex import Latex, latexify, makeupcmd, safecmd
 import os
 
+    
 
-def graphics(sub, node, width):
+def graphics(sub, node, width, border=True):
+    if border:
+        sub.tex('\\setlength\\fboxsep{0pt}\\fbox{')
     sub.graphics_data(node.raw_data, node.mime, width=width, id=node.get_complete_id())
-
+    
+    if border:
+        sub.tex('}')
+    
 def create_techreport_figures(directory, reports):
+    
+    short = ['v_olfaction360o', 'v_optic_unif', 'v_rangefinder180_center']
+
+    all =  short+ ['v_optic_nonunif', 'v_rangefinder_unif', 
+                     'v_rangefinder_nonunif',  'v_rangefinder180_left30',
+                      'v_rangefinder180_front30', 'v_rangefinder180_rot45', 
+                      'v_olfaction180o', 'v_olfaction180n', 
+                      'v_olfaction360n'] # 'v_polarized']
+
+
+    create_techreport_figures_sub(reports, directory, "", short)
+    create_techreport_figures_sub(reports, directory, "extended_", all)
+    
+    
+    
+def create_techreport_figures_sub(reports, directory, prefix, use_vehicles):
     if not os.path.exists(directory):
         os.makedirs(directory)
         
     # create dict vname -> report
     all_vehicles = dict([(report.id, report) for report in reports])
-    
-    use_vehicles =  ['v_optic_unif', 'v_optic_nonunif', 'v_rangefinder_unif', 
-                     'v_rangefinder_nonunif', 
-                     'v_rangefinder180_center', 'v_rangefinder180_left30',
-                      'v_rangefinder180_front30', 'v_rangefinder180_rot45', 
-                      'v_olfaction180o', 'v_olfaction180n', 'v_olfaction360o', 
-                      'v_olfaction360n', 'v_polarized']
-
     not_used = set(all_vehicles.keys()).difference(set(use_vehicles))
     print "Vehicles not used: %s" % list(not_used)
     
-        
-    all_figures = os.path.join(directory, 'all_figures.tex')
-    W = "2.3cm"
+    success_file = '%ssuccess.tex' % prefix
+    tensors_file = '%stensors.tex' % prefix
+    covariance_file = '%scovariance.tex' % prefix
     
-    with Latex.document(all_figures) as doc:
-        doc.use_package('fullpage')
-        doc.input('vehicles_covariance')    
+    all_figures = os.path.join(directory, '%sall_figures.tex' % prefix)
+    W = "1.7cm"
+    interspace="2mm"
+    padding="2.5mm"
+    
+    with Latex.document(all_figures, document_class='svmult') as doc:
+        #doc.use_package('fullpage')
+        #doc.input('vehicles_covariance')    
+        #doc.tex('\\clearpage')
+        doc.input(tensors_file)
         doc.tex('\\clearpage')
-        doc.input('learned_tensors')
-        doc.tex('\\clearpage')
-        doc.input('success')
+        doc.input(success_file)
         
-    vehicles_covariance = os.path.join(directory, 'vehicles_covariance.tex')
+    vehicles_covariance = os.path.join(directory, covariance_file)
     with Latex.fragment(vehicles_covariance, graphics_path=directory) as frag:
         for vname in use_vehicles:
             report = all_vehicles[vname]
@@ -48,8 +67,8 @@ def create_techreport_figures(directory, reports):
             frag.pagebreak()
             with frag.figure(label=label, caption=latexify(vname)) as fig:
                 fig.hfill()
-                with fig.subfigure(caption="Configuration") as sub:
-                    graphics(sub, icon, W)
+                with fig.subfigure(caption="Robot") as sub:
+                    graphics(sub, icon, W,border=False)
                 fig.hfill()
                 with fig.subfigure(caption="Covariance matrix") as sub:
                     graphics(sub, covfig, W)
@@ -60,13 +79,27 @@ def create_techreport_figures(directory, reports):
                     graphics(sub, inffig, W)
                 fig.hfill()
 
-    learned_tensors = os.path.join(directory, 'learned_tensors.tex')
+    learned_tensors = os.path.join(directory, tensors_file)
     with Latex.fragment(learned_tensors, graphics_path=directory) as frag:
+
+        frag.tex( makeupcmd("Mcov") )
+        frag.tex( makeupcmd("Minf") )
+        frag.tex( makeupcmd("Txnat") )
+        frag.tex( makeupcmd("Tynat") )
+        frag.tex( makeupcmd("Tthetanat") )
+        frag.tex( makeupcmd("Txnorm") )
+        frag.tex( makeupcmd("Tynorm") )
+        frag.tex( makeupcmd("Tthetanorm") )
+        
+
         for vname in use_vehicles:
             report = all_vehicles[vname]
             
             icon = report.resolve_url('vehicle-icon/icon')
             
+            covfig = report.resolve_url('covariance/posneg')
+            inffig =  report.resolve_url('information/posneg')
+       
             Tx_nat = report.resolve_url('tensors-natural/tensors/Tx/posneg')
             Ty_nat = report.resolve_url('tensors-natural/tensors/Ty/posneg')
             Ttheta_nat = report.resolve_url('tensors-natural/tensors/Ttheta/posneg')
@@ -76,38 +109,82 @@ def create_techreport_figures(directory, reports):
             Ttheta_norm = report.resolve_url('tensors-normalized/tensors/Ttheta/posneg')
              
             # TODO: add information
+        
+            captioncmd = "%sCAPTIONTENS" % safecmd(vname)
+            frag.tex( makeupcmd(captioncmd) )
             label = "%s:tensors" % vname
             
-            with frag.figure(label=label, caption=latexify(vname)) as fig:
-                with fig.subfigure(caption="Configuration") as sub:
-                    graphics(sub, icon, W)
+            with frag.figure(label=label, caption="\\%s"%captioncmd, placement="p") as fig:
                 fig.hfill()
-                with fig.subfigure(caption="$T_x$") as sub:
+                with fig.subfigure(caption="Robot") as sub:
+                    graphics(sub, icon, W, border=False)
+                fig.hfill()
+                
+                with fig.subfigure(caption="\\Txnat") as sub:
+                    sub.hspace(padding)
                     graphics(sub, Tx_nat, W)
-                fig.hfill()
-                with fig.subfigure(caption="$T_y$") as sub:
+                    sub.hspace(padding)
+                fig.hspace(interspace)
+                with fig.subfigure(caption="\\Tynat") as sub:
+                    sub.hspace(padding)
                     graphics(sub, Ty_nat, W)
-                fig.hfill()
-                with fig.subfigure(caption="$T_\\theta$") as sub:
+                    sub.hspace(padding)
+                fig.hspace(interspace)
+                with fig.subfigure(caption="\\Tthetanat") as sub:
+                    sub.hspace(padding)
                     graphics(sub, Ttheta_nat, W)
-                fig.hfill()
+                    sub.hspace(padding)
+                
+                # I don't know why it makes things uneven
+                #fig.hspace(interspace)
                 
                 fig.parbreak()
+            
+                
+                fig.hfill()
+                fig.hspace("-4mm")
+                with fig.subfigure(caption="\\Mcov") as sub:
+                    graphics(sub, covfig, W)
+                fig.hspace(interspace)
+                with fig.subfigure(caption="\\Minf") as sub:
+                    graphics(sub, inffig, W)
+                
                 fig.hfill()
                 
-                with fig.subfigure(caption="$T_x$ (norm)") as sub:
+                
+                with fig.subfigure(caption="\\Txnorm") as sub:
+                    sub.hspace(padding)
                     graphics(sub, Tx_norm, W)
-                fig.hfill()
-                with fig.subfigure(caption="$T_y$ (norm)") as sub:
+                    sub.hspace(padding)
+                fig.hspace(interspace)
+                with fig.subfigure(caption="\\Tynorm") as sub:
+                    sub.hspace(padding)
                     graphics(sub, Ty_norm, W)
-                fig.hfill()
-                with fig.subfigure(caption="$T_\\theta$ (norm)") as sub:
+                    sub.hspace(padding)
+                fig.hspace(interspace)
+                with fig.subfigure(caption="\\Tthetanorm") as sub:
+                    sub.hspace(padding)
                     graphics(sub, Ttheta_norm, W)
-                fig.hfill()
+                    sub.hspace(padding)
+                fig.hspace(interspace)
                 
-    success = os.path.join(directory, 'success.tex')
+    success = os.path.join(directory, success_file)
     with Latex.fragment(success, graphics_path=directory) as frag:
-        for vname in use_vehicles:
+        
+        frag.tex( makeupcmd("sxynat") )
+        frag.tex( makeupcmd("sytnat") )
+        frag.tex( makeupcmd("sxtnat") )
+        
+        frag.tex( makeupcmd("sxynorm") )
+        frag.tex( makeupcmd("sytnorm") )
+        frag.tex( makeupcmd("sxtnorm") )
+        
+        
+        frag.tex( makeupcmd("sucfigA") )
+        frag.tex( makeupcmd("sucfigB") )
+        frag.tex( makeupcmd("sucfigC") )
+        
+        for i, vname in enumerate(use_vehicles):
             report = all_vehicles[vname]
                     
             icon = report.resolve_url('vehicle-icon/icon')
@@ -121,35 +198,57 @@ def create_techreport_figures(directory, reports):
              
             # TODO: add information
             label = "%s:success" % vname
+        
+            captioncmd = "%sCAPTIONSUCC" % safecmd(vname)
+            frag.tex( makeupcmd(captioncmd) )
+            label = "%s:success" % vname
             
-            with frag.figure(label=label, caption=latexify(vname)) as fig:
-                with fig.subfigure(caption="Configuration") as sub:
-                    graphics(sub, icon, W)
+            with frag.figure(label=label, caption="\\%s"%captioncmd, placement="p") as fig:
                 fig.hfill()
-                with fig.subfigure(caption="$x,y$") as sub:
+                #with fig.subfigure(caption="Robot") as sub:
+                graphics(fig, icon, W, border=False)
+                fig.hfill()
+                
+                with fig.subfigure(caption="\\sxynat") as sub:
+                    sub.hspace(padding)
                     graphics(sub, succ_xy_nat, W)
-                fig.hfill()
-                with fig.subfigure(caption="$y,\\theta$") as sub:
+                    sub.hspace(padding)
+                fig.hspace(interspace)
+                with fig.subfigure(caption="\\sytnat") as sub:
+                    sub.hspace(padding)
                     graphics(sub, succ_yt_nat, W)
-                fig.hfill()
-                with fig.subfigure(caption="$x,\\theta$") as sub:
+                    sub.hspace(padding)
+                fig.hspace(interspace)
+                with fig.subfigure(caption="\\sxtnat") as sub:
+                    sub.hspace(padding)
                     graphics(sub, succ_xt_nat, W)
-                fig.hfill()
+                    sub.hspace(padding)
+                #fig.hspace(interspace)
                 
                 fig.parbreak()
                 fig.hfill()
                 
-                with fig.subfigure(caption="$x,y$") as sub:
-                    graphics(sub, succ_xy_norm, W)
-                fig.hfill()
-                with fig.subfigure(caption="$y,\\theta$") as sub:
-                    graphics(sub, succ_yt_norm, W)
-                fig.hfill()
-                with fig.subfigure(caption="$x,\\theta$") as sub:
-                    graphics(sub, succ_xt_norm, W)
-                fig.hfill()
-     
+                letter = ['A','B','C']
+                fig.tex('\\sucfig%s' % letter[i % 3])
                 
+                with fig.subfigure(caption="\\sxynorm") as sub:
+                    sub.hspace(padding)
+                    graphics(sub, succ_xy_norm, W)
+                    sub.hspace(padding)
+                fig.hspace(interspace)
+                with fig.subfigure(caption="\\sytnorm") as sub:
+                    sub.hspace(padding)
+                    graphics(sub, succ_yt_norm, W)
+                    sub.hspace(padding)
+                fig.hspace(interspace)
+                with fig.subfigure(caption="\\sxtnorm") as sub:
+                    sub.hspace(padding)
+                    graphics(sub, succ_xt_norm, W)
+                    sub.hspace(padding)
+                fig.hspace(interspace)
+         
+                #if i % 3 == 2:
+                #   fig.vspace("-1cm")
 
 
 
